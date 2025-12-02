789d466c785d05c58c392169b2403f4af7dbc110
KenshinRR <krreblando15@gmail.com>
2025-12-02 19:53:32 +0800

    Added Player Data class/table

---
+++ Assets/Scripts/Data Classes/PlayerData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Assets.Scripts.Data_Classes
+{
+    [Table("Player")]
+    public class PlayerData
+    {
+        [PrimaryKey]
+        [Column("Player ID")]
+        public int Player_ID
+        { get; set; }
+
+        [Column("Player Name")]
+        public string Player_Name
+        { get; set; }
+    }
+}
