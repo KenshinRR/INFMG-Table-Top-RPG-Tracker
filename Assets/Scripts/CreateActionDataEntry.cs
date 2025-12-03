62f2b89021f19c44d675526aa19c8d0571d2d736
Za13th <156149739+Za13th@users.noreply.github.com>
2025-12-04 00:04:22 +0800

    Finished Creation
    I changed the database file name from MyDB or whatever to Database.

--- Assets/Scripts/CreateActionDataEntry.cs
+++ Assets/Scripts/CreateActionDataEntry.cs
@@ -8,18 +8,19 @@ public class CreateActionDataEntry : MonoBehaviour
 {
     [SerializeField] public TableCreator tableCreator;
 
-    [SerializeField] public TMP_Text CharacterIDText;
+    [SerializeField] public TMP_InputField CharacterIDText;
     [SerializeField] public CharacterData characterData = null;
     [SerializeField] private bool characterDataAssigned = false;
 
-    [SerializeField] public TMP_Text actionNameText;
-    [SerializeField] public TMP_Text actionDescText;
+    [SerializeField] public TMP_InputField actionNameText;
+    [SerializeField] public TMP_InputField actionDescText;
 
     [Tooltip("If true, uses the text directly placed inside the script instead of the ones attached to the UI.")]
     public bool debugMode = false;
 
     public void AssignCharacterData()
     {
+        characterDataAssigned = false;
         if (int.TryParse(CharacterIDText.text, out int CharacterID))
         {
             this.characterData = this.tableCreator.database.Find<CharacterData>(CharacterID);
@@ -33,13 +34,13 @@ public class CreateActionDataEntry : MonoBehaviour
         }
         else
             Debug.LogError("Failed to parse Character ID input as integer.");
-        this.characterData = this.tableCreator.database.Find<CharacterData>(CharacterID);
 
     }
 
     public void createActionData()
     {
-        if(debugMode)
+        AssignCharacterData();
+        if (debugMode)
         tableCreator.AddActionDataEntry(0, "Attack", "Roll a d20 and add your attack bonus to determine if you hit the target.");
         else if (this.characterDataAssigned)
         {
