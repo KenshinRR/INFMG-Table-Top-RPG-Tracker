66841924e0c8d7fcf3f43848a5b7f92ba86b0056
KenshinRR <krreblando15@gmail.com>
2025-12-04 02:20:08 +0800

    Added a function to Insert Initial Data
    Uncomment the InsertData(); if need be

--- Assets/Scripts/SQLCommanderHelper.cs
+++ Assets/Scripts/SQLCommanderHelper.cs
@@ -27,14 +27,14 @@ public class SQLCommanderHelper : MonoBehaviour
             _do_it = false;
 
             //insert
-            string query =
-                $"INSERT INTO Characters (CharacterName) " +
-                $"VALUES ('Jorge');";
+            //string query =
+            //    $"INSERT INTO Characters (CharacterName) " +
+            //    $"VALUES ('Jorge');";
 
-            database.Query<CharacterData>(query);
+            //database.Query<CharacterData>(query);
 
 
-            Debug.Log($"Added new entry");
+            //Debug.Log($"Added new entry");
 
             //string query =
             //    "SELECT C.CampaignID, CampaignName\r\n" +
@@ -45,8 +45,26 @@ public class SQLCommanderHelper : MonoBehaviour
 
             //database.Query<CampaignData>(query);
 
-
+            //InsertInitialData();
             Debug.Log($"Added new entry");
         }
+    }
+
+    private void InsertInitialData()
+    {
+        database.Execute("INSERT INTO Actions (ActionID, ActionName, ActionDescription)\r\nVALUES (0, 'Yell', 'Causes another person to move faster');\r\n");
+        database.Execute("INSERT INTO CampaignPlayers (RelationID, CampaignID, PlayerID)\r\nVALUES (0, 0, 0);\r\n");
+        database.Execute("INSERT INTO CampaignSessions (RelationID, CampaignID, SessionID)\r\nVALUES (0, 0, 0);");
+        database.Execute("INSERT INTO Campaigns (CampaignID, CampaignName, RuleSystem)\r\nVALUES (0, 'The Beginning of the End', 'Dungeons & Dragons 5e');");
+        database.Execute("INSERT INTO CharacterActions (RelationID, CharacterID, ActionID)\r\nVALUES (0, 0, 0);");
+        database.Execute("INSERT INTO CharacterItems (RelationID, CharacterID, ItemID, Quantity)\r\nVALUES (0, 0, 0, 2);");
+        database.Execute("INSERT INTO Characters (CharacterID, CharacterType, CharacterName, Level, Strength, Vitality, Speed, Defence)\r\nVALUES (0, 'NPC', 'Freddy', 1, 10, 12, 9, 11);");
+        database.Execute("INSERT INTO Items (ItemID, ItemName, ItemDescription)\r\nVALUES (0, 'Coin', 'Used to buy stuff');");
+        database.Execute("INSERT INTO Log_Entries (LogID, Description0, Description1, Dice, DiceResult)\r\nVALUES (0, 'The beginning', 'The adventure begins in a tavern', 0, 0);");
+        database.Execute("INSERT INTO PlayerCharacters (RelationID, PlayerID, CharacterID)\r\nVALUES (0, 0, 0);");
+        database.Execute("INSERT INTO Players (PlayerID, PlayerName)\r\nVALUES (0, 'Jay');");
+        database.Execute("INSERT INTO SessionLogEntries (RelationID, SessionID, LogID)\r\nVALUES (0, 0, 0);\r\n");
+        database.Execute("INSERT INTO Session_Logs (SessionID, Date, Duration, Summary)\r\nVALUES (0, '03/12/2025 12:00:00 AM', 1.5, 'The tavern hustles and bustles');");
+    
     }
 }
